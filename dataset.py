879a7711136c96b53a0f917a6e75c3d44b7fd322
annejooyun <ajmarth@stud.ntnu.no>
2022-11-12 19:07:21 +0100

    Cleaned up dataset

--- dataset.py
+++ dataset.py
@@ -4,26 +4,6 @@ import numpy as np
 import pandas as pd
 import variables as v
 
-DIR_RAW = 'Data/raw_data'
-DIR_FILTERED = 'Data/filtered_data'
-DIR_ICA_FILTERED = 'Data/ica_filtered_data'
-DIR_ICA_FILTERED_2 = 'Data/ica_filtered_data_2'
-
-LABELS_PATH = 'Data/scales.xls'
-
-COLUMNS_TO_RENAME = {
-    'Subject No.': 'subject_no',
-    'Trial_1': 't1_math',
-    'Unnamed: 2': 't1_symmetry',
-    'Unnamed: 3': 't1_stroop',
-    'Trial_2': 't2_math',
-    'Unnamed: 5': 't2_symmetry',
-    'Unnamed: 6': 't2_stroop',
-    'Trial_3': 't3_math',
-    'Unnamed: 8': 't3_symmetry',
-    'Unnamed: 9': 't3_stroop'
-}
-
 
 def load_dataset(data_type="ica2", test_type="Arithmetic"):
     '''
@@ -92,3 +72,16 @@ def convert_to_epochs(dataset, channels, sfreq):
         for j in range(dataset.shape[2]//sfreq):
             epoched_dataset[i, j] = dataset[i, :, j*sfreq:(j+1)*sfreq]
     return epoched_dataset
+
+def get_channels():
+        coordinates_file = os.path.join(self.root, "Coordinates.locs") 
+
+        channel_names = []
+
+        with open(coordinates_file, "r") as file:
+            for line in file:
+                elements = line.split()
+                channel = elements[-1]
+                channel_names.append(channel)
+                
+        return channel_names
