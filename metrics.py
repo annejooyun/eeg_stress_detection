80d84028fce21962dd1a611c3d4b5643273b2725
annejooyun <ajmarth@stud.ntnu.no>
2022-11-12 15:08:55 +0100

    Metrics file made

---
+++ metrics.py
@@ -0,0 +1,21 @@
+from sklearn import metrics
+ 
+def compute_metrics(y_true, y_pred):
+    print(metrics.classification_report(y_true, y_pred))
+
+    conf_matrix = metrics.confusion_matrix(y_true, y_pred)
+    print('\n Confusion matrix:')
+    print(conf_matrix)
+
+    TP = conf_matrix[0,0]
+    TN = conf_matrix[1,1]
+    FN = conf_matrix[1,0]
+    FP = conf_matrix[0,1]
+
+    # Sensitivity = TP / (TP + FN)
+    sensitivity = TP/(TP+FN)
+    print(f'\n Sensitivity: {sensitivity}')
+
+    # Specificity = TN / (TN + FP)
+    specificity = TN/(TN+FP)
+    print(f'\n Specificity: {specificity}')
