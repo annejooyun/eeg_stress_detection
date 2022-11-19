e3cc6c81b9aaadd8c98ec48951e701c27cef9ccf
annejooyun <ajmarth@stud.ntnu.no>
2022-11-19 22:08:53 +0100

    Ran again with new features

--- genetic_alg.py
+++ genetic_alg.py
@@ -28,16 +28,6 @@ def crossover(parents, offspring_size):
     # The point at which crossover takes place between two parents. Usually it is at the center.
     crossover_point = numpy.uint8(offspring_size[1]/2)
 
-#    for k in range(offspring_size[0]):
-        # Index of the first parent to mate.
-#        parent1_idx = k%parents.shape[0]
-        # Index of the second parent to mate.
-#        parent2_idx = (k+1)%parents.shape[0]
-        # The new offspring will have its first half of its genes taken from the first parent.
- #       offspring[k, 0:crossover_point] = parents[parent1_idx, 0:crossover_point]
-        # The new offspring will have its second half of its genes taken from the second parent.
- #       offspring[k, crossover_point:] = parents[parent2_idx, crossover_point:]
-
     offspring_nr = 0
     for i in range(parents.shape[0]):
         for j in range(i+1, parents.shape[0]):
